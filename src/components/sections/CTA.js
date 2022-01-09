39517e0e7e8bccc05e7332b00d08ae07205335c5
Lakshya Choudhary <lakshya.webDev@gmail.com>
2022-01-09 13:44:05 +0530

    new changes

---
+++ src/components/sections/CTA.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CTA = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default CTA;
---
+++ src/components/sections/CanWeHelp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CanWeHelp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default CanWeHelp;
